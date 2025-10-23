c5efa555a97cbf9b6ae6d6a6affafa1a83ea73f0
yossi shapira <yosi105009@gmail.com>
2025-10-23 09:39:57 +0300

    start

---
+++ game_logic/game.py
@@ -0,0 +1,14 @@
+def create_player(name:str) -> dict:
+    
+    return
+
+
+
+def init_game()->dict:
+    return
+
+
+
+
+def play_round(p1:dict,p2:dict):
+    return
---
+++ utils/deck.py
@@ -0,0 +1,14 @@
+def create_card(rank:str,suite:str) -> dict:
+    return
+
+
+def compare_cards(p1_card:dict, p2_card:dict) -> str:
+    return
+
+
+def create_deck() -> list[dict]:
+    return
+
+
+def shuffle(deck:list[dict]) -> list[dict]:
+    return
